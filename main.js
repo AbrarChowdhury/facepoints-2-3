c07284e90dad62236c685da340acbab1912e4c18
Abrar Shams Chowdhury <abrar.chowdhury99@gmail.com>
2023-05-31 15:28:00 +0600

    include gui controls

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import * as THREE from "three"
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-let camera, scene, renderer, clock, upperBeak,lowerBeak
+import { OrbitControls } from "three/addons/controls/OrbitControls.js"
+let camera, scene, renderer, clock, upperBeak, lowerBeak, gui
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js"
-
+import * as dat from "dat.gui"
 init()
 animate()
 
@@ -13,10 +13,10 @@ function init() {
     0.01,
     10
   )
-  camera.position.set(0,-1,5)
+  camera.position.set(0, -1, 5)
 
   clock = new THREE.Clock()
-
+  gui = new dat.GUI()
   scene = new THREE.Scene()
   scene.background = new THREE.Color(0xffffff)
 
@@ -33,36 +33,38 @@ function init() {
   const material = new THREE.MeshStandardMaterial({ color: "yellow" })
   const cylinder = new THREE.Mesh(geometry, material)
   scene.add(cylinder)
-//   model
-  const loader = new GLTFLoader();
-  loader.load( './bird.glb', function ( gltf ) {
+  //   model
+  const loader = new GLTFLoader()
+  loader.load("./bird.glb", function (gltf) {
+    const model = gltf.scene
+    console.log(model)
+    model.traverse((o) => {
+      if (o.isMesh) o.material = material
+    })
+    upperBeak = model.getObjectByName("upper-beak")
+    // upperBeak.rotation.z = -0.05
+    gui
+      .add(upperBeak.rotation, "z", 0,1)
+      .name("upper-beak-rotation")
+    lowerBeak = model.getObjectByName("lower-beak")
+	gui
+	.add(lowerBeak.rotation, "z", -1,0)
+	.name("lower-beak-rotation")
 
-  	const model = gltf.scene;
-	console.log(model)
-	model.traverse((o) => {
-		if (o.isMesh) o.material = material;
-	  });
-  	upperBeak = model.getObjectByName( 'upper-beak' );
-	upperBeak.rotation.z = -0.05
-	lowerBeak = model.getObjectByName( 'lower-beak' );
-	lowerBeak.rotation.z = 0.05
-	console.log(upperBeak)
-
-  	scene.add( model );
-
-  } );
+    scene.add(model)
+  })
 
   renderer = new THREE.WebGLRenderer({ antialias: true })
   renderer.setPixelRatio(window.devicePixelRatio)
   renderer.setSize(window.innerWidth, window.innerHeight)
-//   renderer.outputEncoding = THREE.sRGBEncoding
+  //   renderer.outputEncoding = THREE.sRGBEncoding
   document.body.appendChild(renderer.domElement)
 
   window.addEventListener("resize", onWindowResize, false)
 
-    const controls = new OrbitControls(camera, renderer.domElement)
-    controls.target.set(0, 1, 0)
-    controls.update()
+  const controls = new OrbitControls(camera, renderer.domElement)
+  controls.target.set(0, 1, 0)
+  controls.update()
 }
 
 function onWindowResize() {
@@ -77,10 +79,8 @@ function animate() {
 
   const t = clock.getElapsedTime()
 
-  if ( upperBeak ) {
-
-  	// upperBeak.rotation.z += Math.sin( t ) * 0.005;
-
+  if (upperBeak) {
+    // upperBeak.rotation.z += Math.sin( t ) * 0.005;
   }
 
   renderer.render(scene, camera)
